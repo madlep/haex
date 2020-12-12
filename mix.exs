9ed4bc4b56269d4a195e6cddf1c734dee08d878a
Julian Doherty (madlep) <madlep@madlep.com>
2020-12-12 23:25:15 +1100

    elixir 1.11

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Haex.MixProject do
     [
       app: :haex,
       version: "0.1.0",
-      elixir: "~> 1.10",
+      elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
