df7769465c73e80a90c3bf78f2f9cd45cf6961eb
Julian Doherty (madlep) <madlep@madlep.com>
2020-12-12 23:54:03 +1100

    credo

--- mix.exs
+++ mix.exs
@@ -19,7 +19,8 @@ defmodule Haex.MixProject do
 
   defp deps do
     [
-      {:dialyxir, "~> 1.0", only: [:dev], runtime: false}
+      {:dialyxir, "~> 1.0", only: [:dev], runtime: false},
+      {:credo, "~> 1.5", only: [:dev, :test], runtime: false}
     ]
   end
 end
