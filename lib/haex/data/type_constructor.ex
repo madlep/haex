839b5572782f89d1fa342ce61232e7e1ec624a21
Julian Doherty (madlep) <madlep@madlep.com>
2020-12-15 23:59:40 +1100

    extract type constructor building

--- lib/haex/data/type_constructor.ex
+++ lib/haex/data/type_constructor.ex
@@ -3,8 +3,6 @@ defmodule Haex.Data.TypeConstructor do
   builds type constructor module used to implement a data type
   """
   alias __MODULE__, as: T
-  alias Haex.Data.DataConstructor
-  alias Haex.Data.DataConstructorBuilder
 
   @type t() :: %T{
           name: mod_name(),
@@ -15,14 +13,4 @@ defmodule Haex.Data.TypeConstructor do
 
   @type mod_name() :: [atom()]
   @type param_name() :: atom()
-
-  @spec build(t(), [DataConstructor.t()]) :: Macro.output()
-  def build(%T{name: name}, data_constructors) do
-    quote do
-      defmodule unquote({:__aliases__, [alias: false], name}) do
-        unquote(Enum.map(data_constructors, &DataConstructorBuilder.build/1))
-        unquote(Enum.map(data_constructors, &DataConstructorBuilder.build_helper/1))
-      end
-    end
-  end
 end
---
+++ lib/haex/data/type_constructor_builder.ex
@@ -0,0 +1,20 @@
+defmodule Haex.Data.TypeConstructorBuilder do
+  @moduledoc """
+  generates AST representation of `Haex.Data.TypeConstructor` to return from
+  `Haex.data/1` macro
+  """
+
+  alias Haex.Data.DataConstructor
+  alias Haex.Data.DataConstructorBuilder
+  alias Haex.Data.TypeConstructor
+
+  @spec build(TypeConstructor.t(), [DataConstructor.t()]) :: Macro.output()
+  def build(%TypeConstructor{name: name}, data_constructors) do
+    quote do
+      defmodule unquote({:__aliases__, [alias: false], name}) do
+        unquote(Enum.map(data_constructors, &DataConstructorBuilder.build/1))
+        unquote(Enum.map(data_constructors, &DataConstructorBuilder.build_helper/1))
+      end
+    end
+  end
+end
